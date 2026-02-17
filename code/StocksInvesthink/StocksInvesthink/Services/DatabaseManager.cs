f707f7a8b966283f2a202cef241e6009eb81598f
Sergio Acosta <sergioalasalle@gmail.com>
2026-02-17 08:35:18 -0500

    Adding Singleton

---
+++ code/StocksInvesthink/StocksInvesthink/Services/DatabaseManager.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+
+namespace StocksInvesthink.Services
+{
+    public class DatabaseManager : IDatabaseManager
+    {
+        private readonly string _connectionString;
+
+        public DatabaseManager(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public SqliteConnection GetConnection()
+        {
+            return new SqliteConnection(_connectionString);
+        }
+    }
+}
---
+++ code/StocksInvesthink/StocksInvesthink/Services/IDatabaseManager.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace StocksInvesthink.Services
+{
+    public interface IDatabaseManager
+    {
+        SqliteConnection GetConnection();
+    }
+}
+
